3f5a1ace931a396d1686a42861a372a1072b57a5
Rowan Quinn Whelan <rWhelan340@gmail.com>
2024-11-18 15:12:46 -0500

    update v0.2:added basic bot functionality (warning, this version of the game is pretty unplayable and slow :(. Play at your own risk!

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import random
 from flask import Flask, render_template, request 
 from game_methods import generate_board
+from clue_generator import clue_generator
+import nltk
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    nltk.download('wordnet')
     return render_template("index.html")
 
 @app.route("/game", methods=["GET", "POST"])
@@ -15,7 +19,14 @@ def generate_game():
 @app.route("/practice_guesser", methods=["GET", "POST"])
 def generate_prac_gues_game():
     board = generate_board('data/words.csv')
-    return render_template("practice_guesser.html", grid=board)
+    cg_red = clue_generator(board=board, team=0)
+    cg_blue = clue_generator(board=board, team=1)
+    turn = random.randint(0,1)
+    if turn == 0:
+        clue = cg_red.give_clue()
+    else:
+        clue = cg_blue.give_clue()
+    return render_template("practice_guesser.html", grid=board, turn=turn, clue=clue)
 
 @app.route("/practice_giver", methods=["GET", "POST"])
 def generate_prac_give_game():
--- clue_generator.py
+++ clue_generator.py
@@ -1,39 +1,25 @@
 from enum import Enum
 import gensim.downloader as api
 #import spacy
-from nltk import wordnet
+from nltk.corpus import wordnet
 
 class words(Enum):
     IN = 0
     OUT = 1
 
 class clue_generator:
-    def __init__(self, team):
-        self.board = None
-        self.team = None
-    def give_clue(self):
-        '''
-        this method will look at the given board state and try to find a clue among the synonims of words on the board that scores the highest based on the criteria defined in the score clue method
-        input:
-            none
-        output:
-            clue, a string that the algorithm thinks is the best given the current board state
-        '''
-        pass
-    
-    def score_clue(self, clue):
-        '''
-        the idea of this method is that every clue that could possibly given should be given a score. This helps transform the abstract idea of word similarity to a value the computers can reason about.
-        input: 
-            clue, a string representing the clue the robot is supposed to reason about
+    def __init__(self, board, team, limit=5):
+        self.board = board
+        self.team = team
+        list = []
+        for word in board:
+            if word[1] == self.team:
+                list.append(word[0])
+        self.word_list = list
+        self.limit = limit
         
-        output: 
-            score, a double value ranging from 0-1. Where 0 is a clue which gives zero information gain and 1 is a clue which gives enough information for the guesser to get the remaining clues. 
-            because the number of words each team has left is public knowledge this allows the robot to effectively reason about which clues are effective.
-        '''
-        pass
-    
-    def related(clue, word):
+        
+    def related(self, clue, word):
         '''
         this method is a single version of the score_clue method, where this methods goal is to score how likely a clue is to give the guesser a word. 
         input:
@@ -41,10 +27,9 @@ class clue_generator:
             word, a given word in play on the board
         output:
             probability, a double value representing the likelihood of the given clue making the given word guessable
-        '''
-        
+        '''       
         #After a bit of research I have found three methods to detect the similarity of words, I won't reason about their effectiveness right now but I will later in the projec twhen teh rest of the architecture is built up enough that this reasoning is based on more that intuition
-        #GENSIM
+        """#GENSIM
         model = api.load("glove-wiki-gigaword-50") 
 
         def gensim_word_similarity(word1, word2):
@@ -52,7 +37,7 @@ class clue_generator:
                 return model.similarity(word1, word2)
             except KeyError:
                 return 0
-        
+        """
         """#SPACY
         nlp = spacy.load("en_core_web_sm")
 
@@ -62,7 +47,7 @@ class clue_generator:
             return token1.similarity(token2)"""
 
         #NLTK
-        def word_similarity(word1, word2):
+        def nltk_word_similarity(word1, word2):
             synsets1 = wordnet.synsets(word1)
             synsets2 = wordnet.synsets(word2)
 
@@ -72,3 +57,69 @@ class clue_generator:
             else:
                 return 0
         #FOR RIGHT NOW: I'm going to use NLTK because that happens to correspond to the data I'm using but I'll do some testing in a later version to see what performs best and gives the best user experience
+        probability = nltk_word_similarity(clue, word)
+        return probability
+    
+    def score_clue(self, clue):
+        '''
+        the idea of this method is that every clue that could possibly given should be given a score. This helps transform the abstract idea of word similarity to a value the computers can reason about.
+        input: 
+            clue, a string representing the clue the robot is supposed to reason about
+        
+        output: 
+            score, (implementation unclear right now, this will be updated once the method is finalized)
+        '''
+        total_relational_score = 0
+        for i in self.board:
+            current_related = self.related(clue, i[0])
+            if i[1] == self.team:
+                total_relational_score += current_related
+            elif i[1] == -1:
+                total_relational_score -= current_related
+            else:
+                continue
+        score = total_relational_score
+        return score
+    
+    def get_synonyms(self, word):
+        '''
+        this method will take a word and return a list of synonyms for a word
+        input: 
+            word, a word we want synomyms for
+
+        output:
+            list, a list of strings which are synonyms for the given word
+        '''
+        synonyms = set()
+        for syn in wordnet.synsets(word[0]):
+            for lemma in syn.lemmas():
+                synonyms.add(lemma.name())  # Add synonym to set (set to avoid duplicates)
+        return list(synonyms)[:self.limit]
+    
+    def give_clue(self):
+        '''
+        this method will look at the given board state and try to find a clue among the synonims of words on the board that scores the highest based on the criteria defined in the score clue method
+        input:
+            none
+        output:
+            clue, a string that the algorithm thinks is the best given the current board state
+        '''
+        best_clue = ''
+        highest_score = -1
+        for word in self.word_list:
+            print(f"Generating clues for '{word}'...")
+            synonyms = self.get_synonyms(word)
+            
+            for synonym in synonyms:
+                score = self.score_clue(synonym)
+                print(f"  Clue: {synonym} - Score: {score}")
+                
+                # Check if the current synonym has a higher score
+                if score > highest_score:
+                    highest_score = score
+                    best_clue = synonym
+        
+        return best_clue
+    
+
+    
